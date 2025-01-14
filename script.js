0b7b3a4d1feff97495e20329af1a264f01a34c3a
Abdurrahman Rasyidi <rasyidialzay@gmail.com>
2025-01-14 09:32:18 +0700

    update

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 const menuGambar =
 document.getElementById('menu-gambar');
 const tampilan =
-document.getElementById('tampilan');
+document.getElementById('menu');
 
 menuGambar.addEventListener('click',
     function() {
         tampilan.style.display = 'block';
+    });
+
+const closeGambar =
+document.getElementById('close');
+const tampilan2 =
+document.getElementById('menu');
+    
+closeGambar.addEventListener('click',
+    function() {
+        tampilan.style.display = 'none';
     });
