c3400694a72180a06f099d46c1f94506114da3ea
Abdurrahman Rasyidi <rasyidialzay@gmail.com>
2025-07-16 18:43:12 +0700

    update

--- DB ke-21.js
+++ DB ke-21.js
@@ -5,6 +5,7 @@ document.getElementById('menu');
 
 menuGambar.addEventListener('click',
     function() {
+        tampilan.classList.add('show');
         tampilan.style.display = 'block';
     });
 
@@ -15,17 +16,11 @@ document.getElementById('menu');
     
 closeGambar.addEventListener('click',
     function() {
-        tampilan.style.display = 'none';
+        tampilan.classList.remove('show');
+        setTimeout(() => {
+            tampilan.style.display = 'none';
+        }, 1000); // Match the transition duration
     });
-
-// Optional: Adjust scroll speed dynamically
-document.querySelector('.carousel-track').addEventListener('mouseenter', function () {
-    this.style.animationPlayState = 'paused';
-  });
-
-  document.querySelector('.carousel-track').addEventListener('mouseleave', function () {
-    this.style.animationPlayState = 'running';
-  });
 
 document.querySelectorAll('img').forEach(img => {
     img.addEventListener('contextmenu',
