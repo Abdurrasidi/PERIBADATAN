6944702584b2eb4c72c9aab8745b6739f7473fc0
Abdurrahman Rasyidi <rasyidialzay@gmail.com>
2025-07-19 11:11:23 +0700

    update

--- json.js
+++ json.js
@@ -1,9 +1,43 @@
 $(document).ready(function() {
-  $.getJSON("data.json", function (data) {
-    let menu = data.deskripsi;
+  // Fungsi untuk render semua data awal
+  function renderAllData(menu) {
+    $('#container-poto-1').html('');
     $.each(menu, function (i, data) {
       $('#container-poto-1').append('<article class="article"><div class="image-upload-1"><a href="'+ data.kategori.toLowerCase() +'.html"><img src="poto upload/'+ data.kategori +'/'+ data.potoutama +'" class="image-upload" /></a></div><a href="'+ data.kategori.toLowerCase() +'.html" class="deskripsi"><div class="deskripsi-poto"><p>'+ data.deskripsiutama +'</p></div></a></article>');
     });
+  }
+  $.getJSON("data.json", function (data) {
+    let menu = data.deskripsi;
+    renderAllData(menu);
+    // Event click pada gambar id refresh untuk reset tampilan
+    $(document).on('click', '#refresh', function() {
+      renderAllData(menu);
+    });
+
+  $('#search-input').on('keypress', function(e) {
+    if (e.which === 13) { // Enter key
+      var searchValue = $(this).val().toLowerCase();
+      $.getJSON('data.json', function(data) {
+        let menu = data.deskripsi;
+        let filtered = menu.filter(function(item) {
+          return item.deskripsiutama.toLowerCase().includes(searchValue) || item.kategori.toLowerCase().includes(searchValue);
+        });
+        if (filtered.length > 0) {
+          $('#container-poto-1').html('');
+          $.each(filtered, function(i, data) {
+            $('#container-poto-1').append('<article class="article"><div class="image-upload-1"><a href="'+ data.kategori.toLowerCase() +'.html"><img src="poto upload/'+ data.kategori +'/'+ data.potoutama +'" class="image-upload" /></a></div><a href="'+ data.kategori.toLowerCase() +'.html" class="deskripsi"><div class="deskripsi-poto"><p>'+ data.deskripsiutama +'</p></div></a></article>');
+          });
+        } else {
+          $('#container-poto-1').html(`
+            <div>
+              <h1>Not Found</h1>
+            </div>  
+          `);
+        }
+      });
+      $(this).val(''); // Kosongkan input setelah pencarian
+    }
+  });
 
     // Tambahkan kode ini setelah gambar dimuat
     $('#container-poto-1').on('contextmenu', 'img', function(e) {
